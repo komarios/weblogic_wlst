21ea6ed87e2ba8f87fdbb4ebd81d547d4337cf2f
komarios <komarios@yahoo.com>
2018-07-09 12:35:17 +0300

    Update and rename create_datasources.sh to createDatasources.sh

--- create_datasources.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd $DOMAIN_HOME/bin
-. ./setDomainEnv.sh
-cd /weblogic/bin
-
-java -classpath ${FMWCONFIG_CLASSPATH} ${MEM_ARGS} ${JVM_D64} ${JAVA_OPTIONS} weblogic.WLST ./create_datasources.py 2>&1
