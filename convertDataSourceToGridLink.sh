301f736d42ae5bd91cad6eb550ccf6393a037c3d
komarios <komarios@yahoo.com>
2018-07-09 12:42:34 +0300

    Delete convertDataSourceToGridLink.sh

--- convertDataSourceToGridLink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-. $DOMAIN_HOME/bin/setDomainEnv.sh
-cd $INFRA_HOME
-
-java -classpath ${FMWCONFIG_CLASSPATH} ${MEM_ARGS} ${JVM_D64} ${JAVA_OPTIONS} weblogic.WLST ./convertDataSourceToGridLink.py 2>&1
