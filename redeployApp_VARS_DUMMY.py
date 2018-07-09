60c248f193b01fdf1c431f076694d148d4b6da2d
komarios <komarios@yahoo.com>
2018-07-09 14:12:28 +0300

    Update redeployApp_VARS_DUMMY.py

--- redeployApp_VARS_DUMMY.py
+++ redeployApp_VARS_DUMMY.py
@@ -1,9 +1,7 @@
-
-admin_user ='weblogic'
-admin_pass ='...'
-admin_url  ='t3://...:7001'
+userConfigFilePath='/weblogic/bin/WLSTSecureConfigFile'
+userKeyFilePath='/weblogic/bin/WLSTSecureKeyFile'
+adminURL='t3s://...:7001'
 
 deploy_dir    =  '/weblogic/deployments'
 app_list      = ['myApp1', 'myApp2']
 deploy_target =  'my_cluster'
-
