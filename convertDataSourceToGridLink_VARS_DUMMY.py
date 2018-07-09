b38ba9920dfde9ae4e290f65e88765de1a4faf86
komarios <komarios@yahoo.com>
2018-07-09 12:32:06 +0300

    Create convertDataSourceToGridLink_VARS_DUMMY.py

---
+++ convertDataSourceToGridLink_VARS_DUMMY.py
@@ -0,0 +1,9 @@
+# Set admin url pointing to AdminServer
+userConfigFilePath='/weblogic/bin/WLSTSecureConfigFile'
+userKeyFilePath='/weblogic/bin/WLSTSecureKeyFile'
+adminUrl='t3s://...:7001'
+
+# Grid Link Configuration Parameters
+url='jdbc:oracle:thin:@DB1:1521/myDB'
+rcuUrl='jdbc:oracle:thin:@DB1:1521/myDB'
+ons_node_list='DB1:6200,DB2:6200,DB3:6200'
